4ab591ae07db4e159131d4ae4ab4110c86c00362
Victor Santana <devvictorsantana@gmail.com>
2025-04-25 10:26:19 -0300

    initial commit

---
+++ src/main/java/com/victorsantana/TechLibrary/TechLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.victorsantana.TechLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechLibraryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/victorsantana/TechLibrary/TechLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorsantana.TechLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
